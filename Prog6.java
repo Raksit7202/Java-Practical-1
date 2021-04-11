fa6e662b1375e26a2a1ff6e52583a4395c358974
Raksit <60496575+Raksit7202@users.noreply.github.com>
2021-04-11 16:10:35 +0530

    Update Prog6.java

--- Prog6.java
+++ Prog6.java
@@ -1,20 +1,27 @@
 import java.util.*;
 class Prog6
 {
-	public static void main()
-	{
-
-	}
+    public static void main(String args[])
+    {
+      overload x = new overload();
+      x.area(5);
+      x.area(11,12);
+      x.area(2.5);
+    }
 }
-public area()
+class overload
 {
-
-}
-public area(,)
-{
-
-}
-public area(,,)
-{
-
+    void area(float x)
+    {
+        System.out.println("the area of the square is "+Math.pow(x, 2)+" sq units");
+    }
+    void area(float x, float y)
+    {
+        System.out.println("the area of the rectangle is "+x*y+" sq units");
+    }
+    void area(double x)
+    {
+        double z = 3.14 * x * x;
+        System.out.println("the area of the circle is "+z+" sq units");
+    }
 }
